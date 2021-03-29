3429f4024d554ed440f5cfe7cda60ef69c996f0d
Ashilanffrq19 <7319679-Ashilanffrq@users.noreply.gitlab.com>
2021-03-29 23:46:46 +0700

    latihan ukk

---
+++ database/migrations/2021_03_29_153523_create_siswas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('nis')->unique();
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
+            $table->string('asal_sekolah');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+}
---
+++ resources/views/siswas/show.blade.php
@@ -0,0 +1,70 @@
+@extends('siswas.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Data</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('siswas.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nis:</strong>
+                {{ $siswa->nis }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                {{ $siswa->nama }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jenis Kelamin:</strong>
+                {{ $siswa->jns_kelamin }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tempat Lahir:</strong>
+                {{ $siswa->temp_lahir }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tanggal Lahir:</strong>
+                {{ $siswa->tgl_lahir }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                {{ $siswa->alamat }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Asal Sekolah:</strong>
+                {{ $siswa->asal_sekolah }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kelas:</strong>
+                {{ $siswa->kelas }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jurusan:</strong>
+                {{ $siswa->jurusan }}
+            </div>
+        </div>
+    </div>
+@endsection
